329e4898fc1c23c29fb2d49fc742a78f575c9f29
Toogii <dashdava@uw.edu>
2020-11-01 17:37:23 -0800

    Some optimization done

--- apiRoutes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (app) {
-
-
-    app.get('/api/projects', (req, res) => {
-      console.log(req);
-    });
-};
--- server.js
+++ server.js
@@ -16,12 +16,6 @@ if (process.env.NODE_ENV === "production") {
 // Send every other request to the React app
 // Define any API routes before this runs
 
-require('./routes/apiRoutes')(app);
-
-app.get("/api/projects", (req, res) => {
-  console.log("Received");
-})
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
