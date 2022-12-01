51ca0c682183672f92843f773b50aa2133d1633b
Ulziitogtokh Dashdavaa <53624923+Toogii2019@users.noreply.github.com>
2022-12-01 15:11:22 -0800

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,12 +5,12 @@ function Home() {
   return (
     <header className="showcase">
         <div className="container showcase-inner">
-            <img src="/img/Toogii_Pics.png" className="my-photo"></img>
+            <img src="/img/Toogii_Pics.png" className="my-photo" alt="pic"></img>
             <h1>Welcome</h1>
             <p>I am Toogii. I am a Network Engineer passionate to code, develope and automate.
                 You can checkout my personal and group projects from the Portfolio page.
             </p>
-            <a href="https://www.linkedin.com/in/toogii-dashdavaa-37851442" target="_blank" className="btn">LINKEDIN</a>
+            <a href="https://www.linkedin.com/in/toogii-dashdavaa-37851442" target="_blank" rel="noreferrer" className="btn">LINKEDIN</a>
         </div>
     </header>
   );
