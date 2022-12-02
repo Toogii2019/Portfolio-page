bdb750056412b4847f931e76e84192597171bb1a
Ulziitogtokh Dashdavaa <53624923+Toogii2019@users.noreply.github.com>
2022-12-01 17:03:34 -0800

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,15 @@
+def buildApp() {
+    echo "npm i --save"
+    sh "npm i --save"
+    echo "cd client && npm i --save"
+    sh "cd client && npm i --save"
+    sh "npm run build"  
+}
+
+
+def deployApp() {
+    echo "Deploying"
+    echo "${APP_VERSION}"
+    echo "${SERVER_CREDENTIALS}"
+    echo "${params.executeTests}"
+}
